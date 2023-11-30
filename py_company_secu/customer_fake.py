12284ff72fddc4b71fd50e859afe8546a3296130
baki-zero <115477517+baki-zero@users.noreply.github.com>
2023-11-30 21:40:16 +0900

    Update customer_fake.py

--- py_company_secu/customer_fake.py
+++ py_company_secu/customer_fake.py
@@ -6,8 +6,9 @@ worksheet = workbook.active
 
 worksheet['A1'] = "이름"
 worksheet['B1'] = "전화번호"
+worksheet['C1'] = "우편번호"
+worksheet['D1'] = "주소"
 worksheet['E1'] = "이메일"
-worksheet['F1'] = "IP주소"
 
 fake = Faker('ko_KR')
 
@@ -18,7 +19,6 @@ for row in range(2, 50): #2행부터 시작해 50줄까지 생성
     worksheet.cell(row=row, column=3, value=fake.postcode())
     worksheet.cell(row=row, column=4, value=fake.address())
     worksheet.cell(row=row, column=5, value=fake.email())
-    worksheet.cell(row=row, column=6, value=fake.ipv4_private())
 
 workbook.save("customer_list.xlsx")
 
